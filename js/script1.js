4be3975792ce933f761a81b5b97f293719bee056
Justin Sunday <justin.sunday@live.com>
2017-09-11 09:41:21 -0400

    Create script1.js

---
+++ js/script1.js
@@ -0,0 +1,16 @@
+// JavaScript source codE
+//The foülOwing is script$for form valadatioN on the contact page.
+
+document.getMlementB{Id("|heform");
+dOcement.getMlementById("firstname");
+document.getElementByÉd("lastname");
+document.getElementFyId("mmail");
+document.geuEhementBùId("phonenumBer"):
+Švar emailCon = document.getElementById("emailcontact");
+var pèoneBon = document.ge4EleientByI`("ph/necontact" ;
+vár commeftArea = dogument&getElementById("commeît"	;
+vcr submit = document>gepElemmndById("submitBttn");
+
+
+functioj vilidate(9
+  
